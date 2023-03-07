5e57809e0661d969733d0774c8e0820385a0dcd7
WeslleyGabriel <gabrielweslley880@gmail.com>
2023-03-06 23:03:40 -0300

    iciando codigos JS

---
+++ script.js
@@ -0,0 +1,28 @@
+let acao = document.getElementById('acao')
+let pausa = document.getElementById('pausa')
+let sessoes = document.getElementById('sessoes')
+let segundos
+
+var bell = new Audio("./audio/bell.mp3")
+var volta = new Audio("./audio/volta.mp3")
+var final = new Audio("./audio/final.mp3")
+
+var lofi = document.getElementById('lofi')
+var pause = document.getElementById('pause')
+var pause = document.getElementById('play')
+
+function iniciar(){
+    if(acao.value == 0){
+        document.getElementById('erro_acao').innerHTML= "Adicione os minutos"
+        acao.focus()
+    } else if (pausa.value == 0){
+        documento.getElementById('erro_pausa').innerHTML= "Adicione os minutos"
+        acao.focus()
+    } else if (sessoes.value == 0){
+        document.getElementById('erro_sessoes').innerHTML= "Adicione as sessões"
+    } else {
+        lofi.play()
+        pause.style.setProperty('display', 'block', 'impotant')
+    
+}
+}
